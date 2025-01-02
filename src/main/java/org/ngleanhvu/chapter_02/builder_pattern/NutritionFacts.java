ce07814bed9034d82795083a1988b98b66a48ec6
AnhVuCoder <vunguyen.020820002@gmail.com>
2025-01-02 23:16:31 +0700

    builder pattern

---
+++ src/main/java/org/ngleanhvu/chapter_02/builder_pattern/NutritionFacts.java
@@ -0,0 +1,75 @@
+package org.ngleanhvu.chapter_02.builder_pattern;
+
+/*
+    BUILDER PATTER
+*/
+public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder {
+        // Required parameters
+        private final int servingSize;
+        private final int servings;
+
+        // Optional parameters
+        // Impossible final because it setter many times
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(final int servingSize, final int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val) {
+            this.calories = val;
+            return this;
+        }
+
+        public Builder fat(int val) {
+            this.fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val) {
+            this.sodium = val;
+            return this;
+        }
+
+        public Builder carbohydrate(int val) {
+            this.carbohydrate = val;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        this.servingSize = builder.servingSize;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
+
+    public static void main(String[] args) {
+        NutritionFacts nutritionFacts = new NutritionFacts.Builder(240, 8)
+                .calories(100)
+                .fat(50)
+                .carbohydrate(60)
+                .sodium(100)
+                .build();
+
+        System.out.println(nutritionFacts.calories);
+    }
+}
